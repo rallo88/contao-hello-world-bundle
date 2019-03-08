d74d86a07c99e7ee884c2b5122bee107be443e38
rallo88\Administrator <engels@acpnet.de>
2019-03-08 14:11:14 +0100

    umbenannt

---
+++ tests/ContaoHelloWorldBundleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace rallo88\ContaoHelloWorlddBundle\Tests;
+
+use rallo88\ContaoHelloWorldBundle\ContaoHelloWorldBundle;
+use PHPUnit\Framework\TestCase;
+
+class ContaoHelloWorldBundleTest extends TestCase
+{
+    public function testCanBeInstantiated()
+    {
+        $bundle = new ContaoHelloWorldBundle();
+
+        $this->assertInstanceOf('rallo88\ContaoHelloWorldBundle\ContaoHelloWorldBundle', $bundle);
+    }
+}
